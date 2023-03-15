bcdb2f28189ee8a6ae218f4999c11df5d0d67037
joe <j50508@gmail.com>
2023-03-15 23:44:42 +0800

    Test codes

--- go.mod
+++ go.mod
@@ -2,9 +2,10 @@ module to_do_api
 
 go 1.20
 
+require github.com/gofiber/fiber/v2 v2.42.0
+
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
-	github.com/gofiber/fiber/v2 v2.42.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/klauspost/compress v1.16.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
--- main.go
+++ main.go
@@ -1,20 +1,13 @@
 package main
 
-import (
-    "log"
-    "github.com/gofiber/fiber/v2"
-    "github.com/gofiber/fiber/v2/middleware/cors"
-)
-
+import "github.com/gofiber/fiber/v2"
 
 func main() {
-    app := fiber.New()
-    app.Use(cors.New())
-    api := app.Group("/api")
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World 👋!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World 👋!")
+	})
 
-    log.Fatel(app.Listen(":3000"))
+	app.Listen(":3000")
 }
