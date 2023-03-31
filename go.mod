36fdb7277d50cc319390c8a84a650f3338bf4ef0
hong <j50508@gmail.com>
2023-03-31 17:58:11 +0800

    test mux codes with ChatGPT

--- go.mod
+++ go.mod
@@ -7,11 +7,15 @@ require github.com/gofiber/fiber/v2 v2.42.0
 require (
 	github.com/andybalholm/brotli v1.0.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/gorilla/mux v1.8.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/klauspost/compress v1.15.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.17 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/philhofer/fwd v1.1.1 // indirect
+	github.com/posener/complete v1.2.3 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
 	github.com/savsgio/gotils v0.0.0-20220530130905-52f3993e8d6d // indirect
--- main.go
+++ main.go
@@ -1,13 +1,40 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+    "encoding/json"
+    "log"
+    "net/http"
+
+    "github.com/gorilla/mux"
+)
+
+type Task struct {
+    ID        string `json:"id,omitempty"`
+    Name      string `json:"name,omitempty"`
+    Completed bool   `json:"completed,omitempty"`
+}
+
+var tasks []Task
+
+func GetTasks(w http.ResponseWriter, r *http.Request) {
+    json.NewEncoder(w).Encode(tasks)
+}
+
+func CreateTask(w http.ResponseWriter, r *http.Request) {
+    var task Task
+    _ = json.NewDecoder(r.Body).Decode(&task)
+    tasks = append(tasks, task)
+    json.NewEncoder(w).Encode(task)
+}
 
 func main() {
-	app := fiber.New()
+    router := mux.NewRouter()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Hello, World 👋!")
-	})
+    tasks = append(tasks, Task{ID: "1", Name: "Task 1", Completed: false})
+    tasks = append(tasks, Task{ID: "2", Name: "Task 2", Completed: true})
 
-	app.Listen(":3000")
+    router.HandleFunc("/tasks", GetTasks).Methods("GET")
+    router.HandleFunc("/tasks", CreateTask).Methods("POST")
+
+    log.Fatal(http.ListenAndServe(":8000", router))
 }
